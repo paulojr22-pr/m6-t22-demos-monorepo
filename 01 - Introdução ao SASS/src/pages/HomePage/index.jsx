f8d59b7875789b841283dba2e1ee5f75457b833e
Chrystian Rodolfo <chrystian@kenzie.com.br>
2024-04-01 10:29:33 -0300

    m6-sp1-demo1 base inicial Kenzie Commerce

--- 01 - Introdução ao SASS/src/pages/HomePage/index.jsx
+++ 01 - Introdução ao SASS/src/pages/HomePage/index.jsx
@@ -1,21 +1,12 @@
-import { useState } from "react";
 import { Header } from "../../components/Header";
 import { ProductSection } from "../../components/sections/ProductSection";
-// import { FinanceSection } from "../../components/sections/FinanceSection";
 
 export const HomePage = () => {
-  const [bagItems, setBagItem] = useState([]);
-
-  const addToBag = (item) => {
-    setBagItem([...bagItems, item]);
-  };
-
-  console.log(bagItems);
   return (
     <>
-      <Header bagItemsCount={bagItems.length} />
+      <Header />
       <main className="container">
-        <ProductSection addToBag={addToBag} />
+        <ProductSection />
       </main>
     </>
   );
